6b8b8d1796310ce71accfbde15238e9efbf82e94
Planque Léo <134111837+Pomeux@users.noreply.github.com>
2024-04-18 03:09:27 +0200

    Update Main.java

--- compression/Main.java
+++ compression/Main.java
@@ -9,21 +9,24 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		System.out.println("Programme Compression Image PGM");
-		
+		//si programme lancer en arguments, premier argument lien de image pgm, 2ème entier pour compression rho
 		if(args.length==2) {
 			
 			
 			try {
 		
 			int p=Integer.parseInt(args[1]);
+			//rho compris entre 0 et 100 
 			if(0>p || p>100) {
-				throw new Exception("");
+				throw new Exception("Rho n'est pas compris entre 0 et 100 ");
 			}
 			
 			Quadtree t=new Quadtree(args[0]);
-			
+				
+			//applique compression lambda
 			Quadtree A=t.compressionLambda(t);
-			
+
+			//applique compression rho
 			Quadtree B=t.compressionRho(p);
 			
 		
@@ -37,17 +40,20 @@ public class Main {
 			System.out.println("taux rho: "+100*((double)B.getN()/(double)t.getN()));
 			
 			System.out.println("Fichier dans résultats "+args[0]+"Lambda" +" et "+args[0]+"Rho"+args[1]);
-			
+
+			//traduit l'arbre lambda en image
 			Quadtree.ArbreToImage(t, A, args[0]+"Lambda");
-			
-			Quadtree.ArbreToImage(t, A, args[0]+"Rho"+args[1]);
+
+			//traduit l'arbre rho en image
+			Quadtree.ArbreToImage(t, B, args[0]+"Rho"+args[1]);
 			}
 			catch(Exception e) {
-				System.out.println("mauvais argument ressayez");
+				System.out.println("pgm mauvais lien");
 			}
 		}
 		
 		else {
+			//cas si le nombre d'argument n'est pas égale à 2, menu 
 			boolean enMarche=true;
 			File[] f=chargerFichier();
 			
@@ -56,30 +62,30 @@ public class Main {
 					System.out.println(f[i].getName()+ ":" + (i+1));
 				}
 			
-			System.out.println("Veuillez choisir une image entre 1 et "+f.length);
+				System.out.println("Veuillez choisir une image entre 1 et "+f.length);
 		
-			int nb=Main.recupererNumero(f.length,1);
+				int nb=Main.recupererNumero(f.length,1);
 		
-		Quadtree tree=new Quadtree(f[nb-1].getName());
+				Quadtree tree=new Quadtree(f[nb-1].getName());
 		
-		System.out.println(tree.toString());
+				System.out.println(tree.toString());
 	
 		
-		int choix=choixCompression();
+				int choix=choixCompression();
 		
 		
-			if(choix==1) 
-				compressionLambda(tree,f[nb-1].getName().substring(0,f[nb-1].getName().length()-4));
-			else if(choix==2)
-				compressionRho(tree,f[nb-1].getName().substring(0,f[nb-1].getName().length()-4));
+				if(choix==1) 
+					compressionLambda(tree,f[nb-1].getName().substring(0,f[nb-1].getName().length()-4));
+				else if(choix==2)
+					compressionRho(tree,f[nb-1].getName().substring(0,f[nb-1].getName().length()-4));
 			
 			
 			
-			System.out.println("1 stop, 2 continuez");
-			int stop=Main.recupererNumero(2,1);
-			if(stop==1) {
-				enMarche=false;
-			} 
+				System.out.println("1 stop, 2 continuez");
+				int stop=Main.recupererNumero(2,1);
+				if(stop==1) {
+					enMarche=false;
+				} 
 			}
 			
 	
